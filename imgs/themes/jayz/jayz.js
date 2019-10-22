d68ff88b4141723e321df93fa058855b757864ac
Soloflex95 <michaelswinslow@gmail.com>
2019-10-21 22:01:32 -0400

    Initial Add of Themes

---
+++ imgs/themes/jayz/jayz.js
@@ -0,0 +1,8 @@
+var jayz_images = [
+	"./imgs/themes/jayz/jayz1.PNG",
+	"./imgs/themes/jayz/jayz2.PNG",
+	"./imgs/themes/jayz/jayz3.PNG",
+	"./imgs/themes/jayz/jayz4.PNG",
+	"./imgs/themes/jayz/jayz5.PNG",
+	"./imgs/themes/jayz/jayz6.PNG"
+];
---
+++ imgs/themes/random/random.js
@@ -0,0 +1,13 @@
+var random_images = [
+	"./imgs/themes/random/biceps.jpg",
+	"./imgs/themes/random/calisthenics.jpg",
+	"./imgs/themes/random/giraffe.jpg",
+	"./imgs/themes/random/possum.jpg",
+	"./imgs/themes/random/spout.jpg",
+	"./imgs/themes/random/bubblehead.jpg",
+	"./imgs/themes/random/contortionist.jpg",
+	"./imgs/themes/random/pirate-mice.jpg",
+	"./imgs/themes/random/racoon.jpg",
+	"./imgs/themes/random/skeleton.jpg",
+	"./imgs/themes/random/stormtroopers.jpg"
+];
